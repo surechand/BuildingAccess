d03a5bc49d60c5747525258f66f9f779632a76ec
Piechota Karol <karolp123p@gmail.com>
2023-02-15 22:21:59 +0100

    refacotred

--- dashboard/app.py
+++ dashboard/app.py
@@ -87,8 +87,6 @@ class Window(QMainWindow):
         self.move(60, 15)
         self.create_menu()
         self.create_tabs()
-        self.create_editor()
-        #self.create_appender()
 
     
     def create_menu(self):
@@ -102,52 +100,26 @@ class Window(QMainWindow):
         self.fileMenu.addAction(self.actionExit)
         
     def create_tabs(self):
-        print("test123")
         self.tabs = QTabWidget()
-        
-        self.tab_1 = QWidget()
-        self.tab_2 = QWidget()
-        self.tab_3 = QWidget()
-        
         if doorList:
             for i in doorList:
                 tabList.append(Tab(qWidget=QWidget(),door=i))
             for i in tabList:
                 self.tabs.addTab(i.qWidget, i.door.doorName)        
         self.setCentralWidget(self.tabs)
-
-
-    def create_editor(self):
-        self.text = ''
-        self.editedFile = None
-        outerLayout = QVBoxLayout(self.tab_2)
-        innerLayout = QHBoxLayout()
-
-        saveButton = QPushButton("Zapisz")
-        saveButton.clicked.connect(self.save)
-
-        clearButton = QPushButton("Wyczyść")
-        clearButton.clicked.connect(self.clearText)
-
-        innerLayout.addWidget(saveButton)
-        innerLayout.addWidget(clearButton)
-
-        self.editor = QPlainTextEdit()
-        outerLayout.addWidget(self.editor)
-        outerLayout.addLayout(innerLayout)
         
     def create_appender(self):
         if doorList:
             for i in tabList:
                 layout = QGridLayout(i.qWidget)
 
-                textInput1Label = QLabel("name: "+i.door.doorName)
-                textInput2Label = QLabel("id: "+i.door.lockId)
-                numericInputLabel = QLabel("name: true" if i.door.isOpen else "name: false")
+                nameLabel = QLabel("name: "+i.door.doorName)
+                idLabel = QLabel("id: "+i.door.lockId)
+                openLabel = QLabel("name: true" if i.door.isOpen else "name: false")
             
-                layout.addWidget(textInput1Label, 0, 0)
-                layout.addWidget(textInput2Label, 1, 0)
-                layout.addWidget(numericInputLabel, 2, 0)
+                layout.addWidget(nameLabel, 0, 0)
+                layout.addWidget(idLabel, 1, 0)
+                layout.addWidget(openLabel, 2, 0)
 
     def openImage(self):
         fileName, selectedFilter = QFileDialog.getOpenFileName(self.tab_1, "Wybierz plik obrazu",  "", "PNG (*.png)")
@@ -157,49 +129,6 @@ class Window(QMainWindow):
             pixmap = QPixmap(fileName)
             label.setPixmap(pixmap.scaled(self.tab_1.width(), self.tab_1.height(), Qt.AspectRatioMode.KeepAspectRatio))
             label.show()
-
-    def clearText(self):
-        print("clear text")
-        self.text = ''
-        self.editor.setPlainText(self.text)
-
-    def openText(self):
-        fileName, selectedFilter = QFileDialog.getOpenFileName(self.tab_2, "Wybierz plik tekstowy",  "", "TXT (*.txt)")
-
-        if fileName:
-            self.editedFile = fileName
-            with open(self.editedFile, 'r') as file:
-                self.text = file.read()
-                print(file.read())
-                self.editor.setPlainText(self.text)
-        
-    def saveText(self):
-        if self.editedFile:
-            self.save()
-        else:
-            self.saveTextAs()
-            
-    def saveTextAs(self):
-        fileName, selectedFilter = QFileDialog.getSaveFileName(self.tab_2, "Wybierz plik tekstowy",  "", "TXT (*.txt)")
-
-        if fileName:
-            self.editedFile = fileName
-            self.save()
-
-    def save(self):
-        text = self.editor.toPlainText()
-        with open(self.editedFile, 'w') as file:
-            file.write(text)
-
-    def clearInput(self):
-        self.textInput1.clear()
-        self.textInput2.clear()
-        self.numericInput.clear()
-        self.numericInput.v
-        self.resultField.clear()
-
-    def append(self):
-        self.resultField.setText(self.textInput1.text() + self.textInput2.text() + str(self.numericInput.value() ))
         
 
 app = QApplication([])
